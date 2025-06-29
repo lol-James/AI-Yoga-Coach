3b2b2aff38717f62b2ce808e0628da2b518c7fa9
lol_James <jean0930818930@gmail.com>
2025-06-29 17:07:43 +0800

    Update SQL database

--- sql/yoga_coach_database.sql
+++ sql/yoga_coach_database.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 
--- 伺服器版本： 10.1.38-MariaDB
--- PHP 版本： 7.3.3
+-- 產生時間： 2025-06-29 11:06:45
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -33,9 +32,9 @@ CREATE TABLE `comment_page` (
   `comment_user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   `comment_date` date NOT NULL,
-  `comment_text` text COLLATE utf8_unicode_ci NOT NULL,
-  `comment_like` int(11) DEFAULT '0',
-  `comment_dislike` int(11) DEFAULT '0'
+  `comment_text` text NOT NULL,
+  `comment_like` int(11) DEFAULT 0,
+  `comment_dislike` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -47,8 +46,17 @@ CREATE TABLE `comment_page` (
 CREATE TABLE `favorite_songs` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `song_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `song_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `favorite_songs`
+--
+
+INSERT INTO `favorite_songs` (`id`, `user_id`, `song_name`) VALUES
+(1, 12, '周杰倫 - 白色風車.mp3'),
+(2, 12, '周杰倫 - 最長的電影.mp3'),
+(3, 12, '周杰倫 - 蒲公英的約定.mp3');
 
 -- --------------------------------------------------------
 
@@ -63,13 +71,13 @@ CREATE TABLE `record_detail` (
   `daily_max_app_opens` int(11) DEFAULT NULL,
   `max_daily_usage_hours` int(11) DEFAULT NULL,
   `min_daily_usage_hours` int(11) DEFAULT NULL,
-  `max_posture_name` text COLLATE utf8_unicode_ci,
+  `max_posture_name` text DEFAULT NULL,
   `max_posture_count` int(11) DEFAULT NULL,
-  `min_posture_name` text COLLATE utf8_unicode_ci,
+  `min_posture_name` text DEFAULT NULL,
   `min_posture_count` int(11) DEFAULT NULL,
   `longest_streak_days` int(11) DEFAULT NULL,
   `posture_id` int(11) DEFAULT NULL,
-  `posture_name` text COLLATE utf8_unicode_ci,
+  `posture_name` text DEFAULT NULL,
   `total_completed` int(11) DEFAULT NULL,
   `max_accuracy` float DEFAULT NULL,
   `min_accuracy` float DEFAULT NULL
@@ -88,7 +96,7 @@ CREATE TABLE `record_picture` (
   `mode` int(11) DEFAULT NULL,
   `posture_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `session_id` text COLLATE utf8_unicode_ci
+  `session_id` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -101,9 +109,9 @@ CREATE TABLE `share_page` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `share_date` date NOT NULL,
-  `share_text` text COLLATE utf8_unicode_ci,
-  `share_content` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `share_like` int(11) DEFAULT '0'
+  `share_text` text DEFAULT NULL,
+  `share_content` varchar(255) DEFAULT NULL,
+  `share_like` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -114,9 +122,27 @@ CREATE TABLE `share_page` (
 
 CREATE TABLE `songs` (
   `song_id` int(11) NOT NULL,
-  `song_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `song_path` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `song_name` varchar(255) NOT NULL,
+  `song_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 傾印資料表的資料 `songs`
+--
+
+INSERT INTO `songs` (`song_id`, `song_name`, `song_path`) VALUES
+(25, '周杰倫 - 七里香.mp3', 'music\\周杰倫 - 七里香.mp3'),
+(26, '周杰倫 - 以父之名.mp3', 'music\\周杰倫 - 以父之名.mp3'),
+(27, '周杰倫 - 白色風車.mp3', 'music\\周杰倫 - 白色風車.mp3'),
+(28, '周杰倫 - 夜曲.mp3', 'music\\周杰倫 - 夜曲.mp3'),
+(29, '周杰倫 - 青花瓷.mp3', 'music\\周杰倫 - 青花瓷.mp3'),
+(30, '周杰倫 - 晴天.mp3', 'music\\周杰倫 - 晴天.mp3'),
+(31, '周杰倫 - 最長的電影.mp3', 'music\\周杰倫 - 最長的電影.mp3'),
+(32, '周杰倫 - 菊花台.mp3', 'music\\周杰倫 - 菊花台.mp3'),
+(33, '周杰倫 - 黑色毛衣.mp3', 'music\\周杰倫 - 黑色毛衣.mp3'),
+(34, '周杰倫 - 蒲公英的約定.mp3', 'music\\周杰倫 - 蒲公英的約定.mp3'),
+(35, '周杰倫 - 髮如雪.mp3', 'music\\周杰倫 - 髮如雪.mp3'),
+(36, '周杰倫 - 擱淺.mp3', 'music\\周杰倫 - 擱淺.mp3');
 
 -- --------------------------------------------------------
 
@@ -126,13 +152,13 @@ CREATE TABLE `songs` (
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
-  `user_account` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `user_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `user_picture` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_account` varchar(50) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_picture` varchar(255) DEFAULT NULL,
   `age` int(11) DEFAULT NULL,
-  `gender` enum('male','female','other') COLLATE utf8_unicode_ci DEFAULT 'other',
+  `gender` enum('male','female','other') DEFAULT 'other',
   `register_date` datetime DEFAULT NULL,
-  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+  `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -153,7 +179,8 @@ INSERT INTO `users` (`user_id`, `user_account`, `user_password`, `user_picture`,
 (15, 'test', '444', 'icons/non user.png', 30, 'other', '2025-06-11 22:50:53', 'tset@gmail.com'),
 (16, 'sun', '1234567', 'icons/non user.png', 32, 'other', '2025-06-12 01:06:09', 'sun@gmail.com'),
 (17, 'w ', '2222', 'icons/non user.png', 20, 'other', '2025-06-12 01:56:49', 'w@gmail.com'),
-(18, 'ui ', '888', 'icons/non user.png', NULL, 'other', '2025-06-12 20:37:00', 'ui@gmail.com');
+(18, 'ui ', '888', 'icons/non user.png', NULL, 'other', '2025-06-12 20:37:00', 'ui@gmail.com'),
+(19, 'lol James', 'loljames123', 'icons/non user.png', NULL, 'other', '2025-06-28 16:06:20', 'loljames123@gmail.com');
 
 --
 -- 已傾印資料表的索引
@@ -171,9 +198,7 @@ ALTER TABLE `comment_page`
 -- 資料表索引 `favorite_songs`
 --
 ALTER TABLE `favorite_songs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`,`song_id`),
-  ADD KEY `song_id` (`song_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 資料表索引 `record_detail`
@@ -211,83 +236,76 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- 在傾印的資料表使用自動增長(AUTO_INCREMENT)
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
 --
--- 使用資料表自動增長(AUTO_INCREMENT) `comment_page`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `comment_page`
 --
 ALTER TABLE `comment_page`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- 使用資料表自動增長(AUTO_INCREMENT) `favorite_songs`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `favorite_songs`
 --
 ALTER TABLE `favorite_songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- 使用資料表自動增長(AUTO_INCREMENT) `record_detail`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `record_detail`
 --
 ALTER TABLE `record_detail`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- 使用資料表自動增長(AUTO_INCREMENT) `record_picture`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `record_picture`
 --
 ALTER TABLE `record_picture`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- 使用資料表自動增長(AUTO_INCREMENT) `share_page`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `share_page`
 --
 ALTER TABLE `share_page`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- 使用資料表自動增長(AUTO_INCREMENT) `songs`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
--- 使用資料表自動增長(AUTO_INCREMENT) `users`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
--- 已傾印資料表的限制(constraint)
+-- 已傾印資料表的限制式
 --
 
 --
--- 資料表的限制(constraint) `comment_page`
+-- 資料表的限制式 `comment_page`
 --
 ALTER TABLE `comment_page`
   ADD CONSTRAINT `comment_page_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `share_page` (`id`),
   ADD CONSTRAINT `comment_page_ibfk_2` FOREIGN KEY (`comment_user_id`) REFERENCES `users` (`user_id`);
 
 --
--- 資料表的限制(constraint) `favorite_songs`
---
-ALTER TABLE `favorite_songs`
-  ADD CONSTRAINT `favorite_songs_ibfk_1` FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `favorite_songs_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
-
---
--- 資料表的限制(constraint) `record_detail`
+-- 資料表的限制式 `record_detail`
 --
 ALTER TABLE `record_detail`
   ADD CONSTRAINT `record_detail_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
--- 資料表的限制(constraint) `record_picture`
+-- 資料表的限制式 `record_picture`
 --
 ALTER TABLE `record_picture`
   ADD CONSTRAINT `record_picture_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
--- 資料表的限制(constraint) `share_page`
+-- 資料表的限制式 `share_page`
 --
 ALTER TABLE `share_page`
   ADD CONSTRAINT `share_page_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
