f3944f47543b2d430a93b6cb1acc89a67de47cd9
lolJames <jean0930818930@gmail.com>
2025-09-02 20:37:42 +0800

    model train完了 自己看著辦

--- YOLO/model_train.py
+++ YOLO/model_train.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO(r'C:\Users\lolJames\OneDrive\桌面\AI Yoga Coach\YOLO\yolo11n.pt')
+    model = YOLO(r'AI-Yoga-Coach\YOLO\yolo11n.pt')
     
      
-    model.train(data=r'C:\Users\lolJames\OneDrive\桌面\AI Yoga Coach\YOLO\data.yaml',
+    model.train(data=r'AI-Yoga-Coach\YOLO\data.yaml',
             mode="detect",
             epochs=200,
             imgsz=640,
+            batch=-1,
             device=0)
     
